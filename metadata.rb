ec195530c2a9b618848ddbdf8fbf895815283cd0
Russell Osborne <russosborn@gmail.com>
2013-12-11 11:54:39 -0500

    Repo Name needs to match recipe

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name "chef-gillius"
+name "chef-gilliusfonts"
 maintainer "Russell Osborne"
 maintainer_email "russell@burningpony.com"
 license "Apache 2.0"
 description "Installs Gillus AFT fonts on ubuntu boxes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.2"
+version "0.0.3"
 
 depends "apt"
 
